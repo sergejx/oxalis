ac348707eadbc60c76e19aaebce9d1d29499bd59
Sergej Chodarev <sergej.chodarev@ya.ru>
2014-08-19 17:10:52 +0200

    Detect if application was installed and set path to resources accordingly

--- oxalis/__init__.py
+++ oxalis/__init__.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 APP_INFO = {
     'name': "Oxalis",
     'version': "0.3.dev",
@@ -5,3 +8,10 @@ APP_INFO = {
     'url': 'http://sergejx.mysteria.cz/oxalis/',
     'copyright': "Copyright © 2005-2014 Sergej Chodarev",
 }
+
+DATA_DIR = os.path.join(sys.prefix, 'share', 'oxalis')
+
+
+def resource_path(*path):
+    """Get full path to application data file."""
+    return os.path.join(DATA_DIR, *path)
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-XDG_DATA_DIRS=data:/usr/share/ PYTHONPATH=. python3 -tt scripts/oxalis
+PYTHONPATH=. python3 -tt scripts/oxalis
