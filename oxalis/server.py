8636a34d1685d1af550521d7a0381bddb112e353
Sergej Chodarev <sergej.chodarev@ya.ru>
2014-11-17 18:05:48 +0100

    Convert preview server into a class

--- oxalis/server.py
+++ oxalis/server.py
@@ -1,9 +1,5 @@
-# Oxalis Web Site Editor
-#
-# This module contains small web server integrated in Oxalis and used for
-# previews
-#
-# Copyright (C) 2006-2007 Sergej Chodarev
+# Oxalis -- A website building tool for Gnome
+# Copyright (C) 2006-2014 Sergej Chodarev
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,46 +21,42 @@ import mimetypes
 import shutil
 from threading import Thread
 
-# Global variables
-site = None
 
 class PreviewServer:
     def __init__(self, site):
-        self.site = site
+        self.RequestHandler.site = site
 
     def start(self):
-        global site
-        site = self.site
-        server_thread = Thread(target=run)
+        server_thread = Thread(target=self.run)
         server_thread.setDaemon(True)
         server_thread.start()
 
+    def run(self):
+        server_address = ('127.0.0.1', 8000)
+        httpd = HTTPServer(server_address, PreviewServer.RequestHandler)
+        httpd.serve_forever()
 
-def run():
-    '''Run the server'''
-    server_address = ('127.0.0.1', 8000)
-    httpd = HTTPServer(server_address, OxalisHTTPRequestHandler)
-    httpd.serve_forever()
+    class RequestHandler(BaseHTTPRequestHandler):
+        site = None
 
-class OxalisHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-            base_path = site.get_url_path()
-            request_path = self.path[1:]
-            request_path = request_path[len(base_path):]
-            full_path = os.path.join(site.directory, request_path)
-            if os.path.isdir(full_path):
-                full_path = os.path.join(full_path, 'index.html')
+        def do_GET(self):
+                base_path = self.site.get_url_path()
+                request_path = self.path[1:]
+                request_path = request_path[len(base_path):]
+                full_path = os.path.join(self.site.directory, request_path)
+                if os.path.isdir(full_path):
+                    full_path = os.path.join(full_path, 'index.html')
 
-            if os.path.exists(full_path):  # Other files
-                mime = mimetypes.guess_type(full_path)
+                if os.path.exists(full_path):  # Other files
+                    mime = mimetypes.guess_type(full_path)
 
-                self.send_response(200)
-                self.send_header('Content-Type', mime[0])
-                self.end_headers()
+                    self.send_response(200)
+                    self.send_header('Content-Type', mime[0])
+                    self.end_headers()
 
-                f = open(full_path, 'rb')
-                shutil.copyfileobj(f, self.wfile)
-                f.close()
+                    f = open(full_path, 'rb')
+                    shutil.copyfileobj(f, self.wfile)
+                    f.close()
 
-            else:  # File not found
-                self.send_error(404, 'Not Found')
+                else:  # File not found
+                    self.send_error(404, 'Not Found')
